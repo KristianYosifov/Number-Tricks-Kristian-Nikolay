569f2d05ed2cee1c55f91013b2eaf1df13d0d93e
MEGA <MEGA@gmail.com>
2025-03-10 11:32:21 +0200

    CREATED CLASS C

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace NumberTricks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.ReadLine();
+        }
+    }
+}
