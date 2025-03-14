2034a9ccab03170953959a23f1f63f8e45b106c4
MEGA <MEGA@gmail.com>
2025-03-14 12:28:19 +0200

    blq

--- NumberUtils.cs
+++ NumberUtils.cs
@@ -2,8 +2,23 @@
 
 public class Class1
 {
-	public Class1()
+	public class NumberUtils
 	{
+		public NumberUtils()
+		{
+
+		}
+
+		public static double FastExponentiate(double a, int power)
+		{
+			if (power == 0) return 1;
+			double halfPowered = FastExponentiate(a, power/2);
+			double subTotal = halfPowered * halfPowered;
+			return power % 2 == 0 ? subTotal : subTotal * a;
+		}
+	}
+
+
         
     }
 
