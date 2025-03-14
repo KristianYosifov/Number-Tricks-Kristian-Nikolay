80aa57f6b511492e3d94e7e0093ccd0bd6e23306
MEGA <MEGA@gmail.com>
2025-03-14 11:45:31 +0200

    Baby blq blq blq

--- NumberUtils.cs
+++ NumberUtils.cs
@@ -4,5 +4,22 @@ public class Class1
 {
 	public Class1()
 	{
-	}
+        
+    }
+
+    
+
+    public static float SquareRoot(float n)
+    {
+
+        float x = n;
+        float y = 1;
+        double e = 0.000001;
+        while (x - y > e)
+        {
+            x = (x + y) / 2;
+            y = n / x;
+        }
+        return x;
+    }
 }
