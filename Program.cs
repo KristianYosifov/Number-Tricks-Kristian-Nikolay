ab1f8b70ddc0d50fc339d89efffef2ca09b8fa9e
MEGA <MEGA@gmail.com>
2025-03-17 11:22:15 +0200

    create EratosthenesSieve

--- Program.cs
+++ Program.cs
@@ -12,7 +12,12 @@
             Console.WriteLine(NumberUtils.SquareRoot(4));         
             Console.ReadKey();
 
-            
+
+            Console.WriteLine("SieveOfEratosthenes:");
+            int n = int.Parse(Console.ReadLine());
+            NumberUtils.SieveOfEratosthenes(n);
+
+
 
         }
     }
